07f677690e4b579f0e47c39c2d2652b3d5f597b6
Umami72 <nzrunitycollab@gmail.com>
2024-07-04 12:26:01 +0700

    updating route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');# Main Page
+});
+
+Route::get("/hello", function () {
+    return "hehe"; 
+})->name("CorrectURL");
+
+Route::get("/hallo", function () {
+    return redirect()->route("CorrectURL"); #link redirect
+})-> Name("test rerouting");
+
+Route::get("greet/{name}", function ($name){
+    return "hello " . ($name);
+});
+
+Route::fallback(function () {
+    return view("welcome"); #route if there is no route available
 });
