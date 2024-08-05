479ba94052c532c963d13d392826a1afa0bcc66f
Umami72 <nzrunitycollab@gmail.com>
2024-08-05 18:34:46 +0700

    added edit form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \App\Models\Task;
 use Faker\Guesser\Name;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -10,18 +11,65 @@ Route::get('/', function () {
 }) -> Name("tasks.index");
 
 Route::get('/tasks',function () {
-  return view('index', ['name' => 'Nizar', 'tasks' => \App\Models\Task::latest()->orderBy('completed') -> get()]); # Main Page
+  return view('index', ['name' => 'Nizar', 'tasks' => Task::latest()->orderBy('completed') -> get()]); # Main Page
 }) -> Name("homepage");
 
 Route::view("/tasks/create", 'create') -> name("tasks.create");
 
+//get task
 Route::get("/tasks/{id}", function($id) {
-  return view("show", ['task'=> \App\Models\Task::findOrFail($id)] );
+  return view("show", ['task'=> Task::findOrFail($id)] );
 }) -> name("tasks.show");
 
+//edit task
+Route::get("/tasks/{id}/edit", function($id) {
+  return view("edit", ['task'=> Task::findOrFail($id)] );
+}) -> name("tasks.edit");
+
+//
+Route::put('/tasks/{id}', function($id, Request $request) {
+  // dd($request -> all()); #check the structure of the POST
+  $data = $request -> validate([
+    "title" => "required | max : 255",
+    "description" => "required",
+    "long_description" => "required"
+  ]);
+
+  $task = Task::findOrFail($id);
+  $task -> title = $data["title"];
+  $task -> description = $data["description"];
+  $task -> long_description = $data["long_description"];
+  $task -> save();
+
+  return redirect() -> route("tasks.show", ["id" => $task -> id ])
+    -> with("success", "Task updated successfully!");
+}) -> name('tasks.update');
+
+//post new task
 Route::post('/tasks', function(Request $request) {
-  dd($request -> all());
+  // dd($request -> all()); #check the structure of the POST
+  $data = $request -> validate([
+    "title" => "required | max : 255",
+    "description" => "required",
+    "long_description" => "required"
+  ]);
+
+  $task = new Task;
+  $task -> title = $data["title"];
+  $task -> description = $data["description"];
+  $task -> long_description = $data["long_description"];
+  $task -> save();
+
+  return redirect() -> route("tasks.show", ['id' => $task->id])
+    -> with("success", "Task created successfully!");
 }) -> name('tasks.store');
+
+Route::fallback(function () {
+    return view("welcome"); #route if there is no route available
+});
+
+
+
 
 // Route::get("/hello", function () {
 //     return "hehe"; 
@@ -34,7 +82,3 @@ Route::post('/tasks', function(Request $request) {
 // Route::get("greet/{name}", function ($name){
 //     return "hello " . ($name);
 // });
-
-Route::fallback(function () {
-    return view("welcome"); #route if there is no route available
-});
