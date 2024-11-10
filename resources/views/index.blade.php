e82d6f79cc143f44af87074ac4e85886bd14543c
Umami72 <nzrunitycollab@gmail.com>
2024-11-10 14:45:14 +0700

    updated some function

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
         <div>There are task to be completed!</div>
         @foreach ($tasks as $task)
         <div>
-            <a href="{{route('tasks.show', ["id" => $task -> id]) }}">{{$task -> title}}</a>
+            <a href="{{route('tasks.show', ["task" => $task -> id]) }}">{{$task -> title}}</a>
         </div>
         @endforeach
     @else
